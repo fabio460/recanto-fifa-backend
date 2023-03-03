5eba065560faf081179138a52812a49a032af0e6
fabio dasilva de oliveira <dasilvadeoliveirafabio@gmail.com>
2023-03-03 16:37:05 -0300

    criados os cruds

--- src/rotas/index.ts
+++ src/rotas/index.ts
@@ -1,8 +1,10 @@
 import { rotaType } from "../../types";
+import jogadorRouter from "./jogadorRouter";
 import usuarioRouter from "./usuarioRouter";
 
 const route:rotaType[] = [
-    {endpoint:"/usuario",rota:usuarioRouter}
+    {endpoint:"/usuario",rota:usuarioRouter},
+    {endpoint:"/jogador",rota:jogadorRouter}
 ]
 
 export default route
--- src/rotas/usuarioRouter.ts
+++ src/rotas/usuarioRouter.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { getUsuario } from "../controllers/usuarioController";
+import { atualizar, criar, deletar, getUsuario, getUsuarioPoId } from "../controllers/usuarioController";
 
 const usuarioRouter = Router()
 
 usuarioRouter.get("/",getUsuario)
-
+usuarioRouter.get("/:id",getUsuarioPoId)
+usuarioRouter.post("/",criar)
+usuarioRouter.put('/',atualizar)
+usuarioRouter.delete('/',deletar)
 export default usuarioRouter
