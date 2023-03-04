4811a141e51272b00d1128d7d6448abbcca62cc8
fabio dasilva de oliveira <dasilvadeoliveirafabio@gmail.com>
2023-03-03 21:45:18 -0300

    teste 2

--- src/controllers/usuarioController.ts
+++ src/controllers/usuarioController.ts
@@ -5,12 +5,12 @@ const prisma = new PrismaClient()
 
 
 export const getUsuario =async (req:Request,res:Response)=>{
-   // const u = await prisma.usuario.findMany({
-   //    include:{
-   //       jogadore:true
-   //    }
-   // })
-   res.json("olaaa")
+   const u = await prisma.usuario.findMany({
+      include:{
+         jogadore:true
+      }
+   })
+   res.json(u)
 }
 
 export const getUsuarioPoId =async (req:Request,res:Response)=>{
