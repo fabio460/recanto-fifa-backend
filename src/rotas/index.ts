fe8c857914f0af66373517622cc27b8d8d9af716
fabio dasilva de oliveira <dasilvadeoliveirafabio@gmail.com>
2023-03-08 09:04:07 -0300

    criado endpoint de temporadas

--- src/rotas/index.ts
+++ src/rotas/index.ts
@@ -1,10 +1,12 @@
 import { rotaType } from "../../types";
 import jogadorRouter from "./jogadorRouter";
+import temporadaRouter from "./temporadaRouter";
 import usuarioRouter from "./usuarioRouter";
 
 const route:rotaType[] = [
     {endpoint:"/usuario",rota:usuarioRouter},
-    {endpoint:"/jogador",rota:jogadorRouter}
+    {endpoint:"/jogador",rota:jogadorRouter},
+    {endpoint:"/temporada",rota:temporadaRouter}
 ]
 
 export default route
